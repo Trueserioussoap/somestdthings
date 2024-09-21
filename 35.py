c4310dd5189b5029c37dcee61f222d6c081b170b
Trueserioussoap <serious_soap@mail.ru>
2024-09-22 03:02:16 +0500

    Update 35.py

--- 35.py
+++ 35.py
@@ -1,5 +1,7 @@
 def get_multiplied_digits (number):
     str_number=str(number)
+    str_number=str_number.replace('0','')
+    #print(str_number)
     if len(str_number)>1:
         first=int(str_number[0])
         return first*get_multiplied_digits(int(str_number[1:]))
@@ -7,4 +9,6 @@ def get_multiplied_digits (number):
         return (int(str_number))
 
 result = get_multiplied_digits(40203)
+print(result)
+result = get_multiplied_digits(420)
 print(result)
